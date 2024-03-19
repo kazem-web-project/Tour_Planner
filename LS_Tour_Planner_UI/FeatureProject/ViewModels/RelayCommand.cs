6e8d321c31408be3f00c37b026fd80e913c8c1ee
CooopeIR <lorenz.stoiber97@outlook.de>
2024-03-19 20:55:31 +0100

    Lorenz Folder gemerged

---
+++ LS_Tour_Planner_UI/FeatureProject/ViewModels/RelayCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FeatureProject.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+
+        readonly Action<object?> _execute;
+        readonly Predicate<object?> _canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<object?> execute) : this(execute, null)
+        {
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (_canExecute != null)
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+    }
+}
